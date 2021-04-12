09b4c8e8ee57761cc30fb7418ffdb1e93a573115
CarlosRivas <crivas1@miuandes.cl>
2021-04-12 20:46:55 -0300

    Menu

--- main.py
+++ main.py
@@ -23,3 +23,38 @@ LPlayers = create_player(x,n) #Keys Dictionary
 for n in LPlayers: #Uso de la funcion Dic en la clase Player
     p = Player(n).Dic()
     LUsers.append(p)
+
+for i in LUsers: #Dicionario con cartas para cada jugador
+    c = maze[0]
+    maze.pop(0)
+    i["cards"].append(c)
+    c = maze[0]
+    maze.pop(0)
+    i["cards"].append(c)
+
+def print_menu(): #Menu Turno
+    print("")
+    for i in LUsers:
+        print(i["name"],"Cartas:", len(i["cards"]), "Cartas Perdidas:",i["lostcards"], "Monedas:",i["coins"])
+    print("")
+    print("Selecione que quiere hacer: ")
+    print("1. Ejecutar una accion")
+    print("2. Ver mis cartas")
+    print("3. Ver log")
+    return int(input())
+
+def game(mazo, LJ, LU, turno):
+    JA = LU[turno]["name"] #acceder al nombre del jugador actual
+    print(JA)
+    selection = print_menu()
+    if selection == 1:
+        pass
+        if turno != len(LJ):
+            turno += 1
+        else:
+            turno = 0
+    if selection == 2:
+        print(LU[turno]["cards"]) #acceder a cartas del jugador actual
+
+turno=0
+game(maze, LPlayers, LUsers, turno)    
--- players.py
+++ players.py
@@ -1,9 +1,7 @@
-from numpy import *
-
 class Player:
     def __init__(self, name):
         self.__name = name
     
     def Dic(self):
-        D = {"name": self.__name , "cards":0, "coins":2, "log":[], "influence":2}
+        D = {"name": self.__name , "cards":[], "coins":2, "log":[], "influence":2, "lostcards":[]}
         return D
